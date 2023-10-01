031d6e2812b883a3ecde1d3bf72bd097d57e76b2
daria kutluieva <daryakutlueva@gmail.com>
2023-10-01 14:09:27 +0200

    random and links to img

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-const slides = document.querySelectorAll(".slide"); //array of slides
-
-for (const slide of slides) {
-  slide.addEventListener("click", () => {
-    clearActiveClasses();
-    slide.classList.add("active");
-  });
+let random;
+function randomNum() {
+  random = Math.floor(Math.random() * 6);
+  console.log(random);
 }
 
-function clearActiveClasses() {
-  slides.forEach((slide) => {
-    slide.classList.remove("active");
-  });
+function slidesPlugin(activeSlide = 0) {
+  const slides = document.querySelectorAll(".slide"); //array of slides
+
+  slides[activeSlide].classList.add("active");
+
+  for (const slide of slides) {
+    slide.addEventListener("click", () => {
+      clearActiveClasses();
+      slide.classList.add("active");
+    });
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove("active");
+    });
+  }
 }
+
+randomNum();
+slidesPlugin(random);
